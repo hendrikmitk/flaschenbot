6dd8866f02f2d3e68dcfe955883e455dae1ac3e1
hendrikmitk <hendrikharlichs@fastmail.com>
2023-07-01 19:02:26 +0200

    Introduce Mastodon client

---
+++ src/client/mastodon.client.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const BASE_URL = 'https://botsin.space';
+const TOKEN = process.env.MASTODON_TOKEN;
+
+export const client = () => {
+  const config = {
+    baseURL: BASE_URL,
+    headers: { Authorization: `Bearer ${TOKEN}` },
+  };
+  return axios.create(config);
+};
+
+export default {
+  postStatus(toot: string) {
+    return client().post(
+      `/api/v1/statuses`,
+      {},
+      {
+        params: { status: toot },
+      }
+    );
+  },
+};
