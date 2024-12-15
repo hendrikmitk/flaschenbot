f227d5108e2c16e080910b3d2aa2abef3a29c2fc
hendrikmitk <hendrikharlichs@fastmail.com>
2024-12-15 11:54:29 +0100

    Refactor articles route to async/await

--- dist/routes/articles.js
+++ dist/routes/articles.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,7 +17,7 @@ const flaschenpost_client_1 = __importDefault(require("../client/flaschenpost.cl
 const get_flaschenpost_offers_rule_1 = require("../rules/get-flaschenpost-offers.rule");
 const get_unique_article_ids_rule_1 = require("../rules/get-unique-article-ids.rule");
 const articles = express_1.default.Router();
-articles.get('/', (req, res) => {
+articles.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const queryParams = req.query;
     if (!('id' in queryParams)) {
         return res.status(422).send({
@@ -18,23 +27,23 @@ articles.get('/', (req, res) => {
             data: undefined,
         });
     }
-    flaschenpost_client_1.default
-        .getArticles((0, get_unique_article_ids_rule_1.getUniqueArticleIdsRule)(queryParams['id']))
-        .then((response) => {
+    try {
+        const articleIds = (0, get_unique_article_ids_rule_1.getUniqueArticleIdsRule)(queryParams['id']);
+        const response = yield flaschenpost_client_1.default.getArticles(articleIds);
         return res.status(200).send({
             code: res.statusCode,
             text: 'OK',
             message: undefined,
             data: (0, get_flaschenpost_offers_rule_1.getFlaschenpostOffersRule)(response.data),
         });
-    })
-        .catch((error) => {
+    }
+    catch (error) {
         return res.status(500).send({
             code: res.statusCode,
             text: 'Internal Server Error',
-            message: 'Failed to load articles from flaschenpost',
-            data: error,
+            message: error instanceof Error ? error.message : 'An unknown error occurred',
+            data: undefined,
         });
-    });
-});
+    }
+}));
 exports.default = articles;
--- src/routes/articles.ts
+++ src/routes/articles.ts
@@ -6,7 +6,7 @@ import { getUniqueArticleIdsRule } from '../rules/get-unique-article-ids.rule';
 
 const articles: Router = express.Router();
 
-articles.get('/', (req: Request, res: Response) => {
+articles.get('/', async (req: Request, res: Response) => {
   const queryParams = req.query;
 
   if (!('id' in queryParams)) {
@@ -18,26 +18,27 @@ articles.get('/', (req: Request, res: Response) => {
     });
   }
 
-  flaschenpostClient
-    .getArticles(
-      getUniqueArticleIdsRule(queryParams['id'] as string[] | string)
-    )
-    .then((response) => {
-      return res.status(200).send({
-        code: res.statusCode,
-        text: 'OK',
-        message: undefined,
-        data: getFlaschenpostOffersRule(response.data),
-      });
-    })
-    .catch((error) => {
-      return res.status(500).send({
-        code: res.statusCode,
-        text: 'Internal Server Error',
-        message: 'Failed to load articles from flaschenpost',
-        data: error,
-      });
+  try {
+    const articleIds = getUniqueArticleIdsRule(
+      queryParams['id'] as string[] | string
+    );
+    const response = await flaschenpostClient.getArticles(articleIds);
+
+    return res.status(200).send({
+      code: res.statusCode,
+      text: 'OK',
+      message: undefined,
+      data: getFlaschenpostOffersRule(response.data),
     });
+  } catch (error: Error | unknown) {
+    return res.status(500).send({
+      code: res.statusCode,
+      text: 'Internal Server Error',
+      message:
+        error instanceof Error ? error.message : 'An unknown error occurred',
+      data: undefined,
+    });
+  }
 });
 
 export default articles;
