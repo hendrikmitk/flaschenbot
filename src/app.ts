af075b2e8bf76733980bf22e9a5f6f50544592a3
hendrikmitk <hendrikharlichs@fastmail.com>
2024-12-15 20:06:00 +0100

    Add default route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 import articles from './routes/articles';
 import combined from './routes/combined';
 import favorites from './routes/favorites';
 import status from './routes/status';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 
 dotenv.config();
 
 const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(StatusCodes.NOT_FOUND).send({
+    code: res.statusCode,
+    text: ReasonPhrases.NOT_FOUND,
+    message: undefined,
+    data: undefined,
+  });
+});
 
 app.use('/articles', articles);
 app.use('/combined', combined);
